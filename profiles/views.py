d82cba45a2d3e056f45ff01bf13b3ba26abb958e
tmarkec <tmarkec@gmail.com>
2023-04-27 21:59:04 +0000

    add functionality to display order history

--- profiles/views.py
+++ profiles/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
 from .forms import UserProfileForm
-
+from django.contrib import messages
+from checkout.models import Order
 
 def profile(request):
     """ Display the user's profile information """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
     form = UserProfileForm(instance=profile)
 
     template = 'profiles/profile.html'
     context = {
-        'form': form
+        'form': form,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
@@ -27,14 +34,27 @@ def info(request):
     return render(request, template, context)
 
 
-def history(request):
-    """ Display the user account options """
-    
-    profile = get_object_or_404(UserProfile, user=request.user)
-    orders = profile.orders.all()
-    template = 'profiles/history.html'
-    context = {
-        'orders': orders
-    }
+# def history(request):
+#     """ Display the user previous orders """
 
+#     orders = profile.orders.all()
+#     template = 'profile/history.html'
+#     context = {
+#         'orders': orders,
+      
+#     }
+
+#     return render(request, template, context)
+def history(request):
+    """Display the user's previous orders"""
+
+    # Retrieve the current user's profile
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    # Retrieve all orders associated with the user's profile
+    orders = Order.objects.filter(user_profile=profile).order_by('-date')
+
+    # Render the template with the list of orders
+    template = 'profiles/history.html'
+    context = {'orders': orders}
     return render(request, template, context)
