4a0c86cd5848b4bf34f4cc7da8b6e9a7c4436cfc
tmarkec <tmarkec@gmail.com>
2023-04-29 20:10:51 +0000

    changed fields on contact form

--- contact/forms.py
+++ contact/forms.py
@@ -6,3 +6,10 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ('fname', 'lname', 'email', 'subject', 'message')
+        labels = {
+            'fname': 'First Name',
+            'lname': 'Last Name',
+            'email': 'Email',
+            'subject': 'Subject',
+            'message': 'Message'
+        }
