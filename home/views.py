fb40322a90eccf53944587b3d7454af3b9edcb84
tmarkec <tmarkec@gmail.com>
2023-05-01 09:11:23 +0000

    add style accross website, add about us page with content

--- home/views.py
+++ home/views.py
@@ -7,3 +7,8 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ A view to return the about us page """
+    return render(request, 'home/about_us.html')
