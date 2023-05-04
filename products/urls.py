2bdbc58d7786f66dd05e1b2b8734d0ec2f514b7b
tmarkec <tmarkec@gmail.com>
2023-05-04 07:18:12 +0000

    add functionality to edit product from shop

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('add_review/<product_id>', views.add_review, name='add_review'),
     path('del_review/<int:review_id>', views.del_review, name='del_review'),
     path('update_review/<int:review_id>', views.update_review, name='update_review'),
