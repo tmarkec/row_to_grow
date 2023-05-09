2627d19ffe1c5441623196051bf231186cb2046f
tmarkec <tmarkec@gmail.com>
2023-05-09 12:04:08 +0000

    add cache control in settings.py

--- row_to_grow/settings.py
+++ row_to_grow/settings.py
@@ -194,6 +194,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # bucket config
     AWS_STORAGE_BUCKET_NAME = 'row-to-grow'
     AWS_S5_REGION_NAME = 'EU (Ireland) eu-west-1'
@@ -210,6 +215,7 @@ if 'USE_AWS' in os.environ:
     # override static and media url in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+
 
 # Stripe payment
 STANDARD_DELIVERY_PERCENTAGE = 10
