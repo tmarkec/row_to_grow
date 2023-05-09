444aa4a2acacac544abed85636be772ef00e64c7
tmarkec <tmarkec@gmail.com>
2023-05-09 09:41:31 +0000

    removed secret key and set debug

--- row_to_grow/settings.py
+++ row_to_grow/settings.py
@@ -22,10 +22,10 @@ if os.path.isfile("env.py"):
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7p=-l9q1=lu$cgu$ex9uvedri1!48_t29*ws3u$ma*l6i!$wm6'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['row-to-grow.herokuapp.com', 'localhost']
 
