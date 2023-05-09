8468e1c243d0a51ec2b2a341851cf149b3ec75c6
tmarkec <tmarkec@gmail.com>
2023-05-09 12:49:31 +0000

    bug typo databeses settings.py

--- row_to_grow/settings.py
+++ row_to_grow/settings.py
@@ -133,14 +133,14 @@ WSGI_APPLICATION = 'row_to_grow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 if 'DATABASE_URL' in os.environ:
-    DATABASE = {
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
