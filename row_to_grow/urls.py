bd137507f7283180e6a61233fa3240a1532f7083
tmarkec <tmarkec@gmail.com>
2023-05-13 14:43:42 +0000

    testing 404 handler

--- row_to_grow/urls.py
+++ row_to_grow/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+# from .views import handler404
 from django.urls import path
-from .views import handler404
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
