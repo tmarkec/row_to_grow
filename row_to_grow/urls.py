71f6445f62307ec6845c2a39e662bbdaf141cf38
tmarkec <tmarkec@gmail.com>
2023-05-13 01:32:09 +0000

    bug 404 not displaying on heroku

--- row_to_grow/urls.py
+++ row_to_grow/urls.py
@@ -17,19 +17,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('contact/', include('contact.urls')),
     path('subscription', include('subscription.urls')),
+    path('test/', views.test_view, name='test'),
+    path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'row_to_grow.views.handler404'
-handler500 = "row_to_grow.views.server_error"
+handler500 = 'row_to_grow.views.server_error'
--- row_to_grow/views.py
+++ row_to_grow/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 
 def handler404(request, exception):
-    """ 
+    """
     This function shows a customized 404 error page
      """
-    return render(request, '404.html', status=404)
+    return render(request, 'errors/404.html', status=404)
 
 
 def server_error(request):
@@ -13,3 +13,7 @@ def server_error(request):
         This function shows a customized 500 error page
     """
     return render(request, 'errors/500.html', status=500)
+
+
+def test_view(request):
+    return render(request, 'test.html')
