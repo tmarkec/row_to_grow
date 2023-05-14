3e313c3e482d64a3da8d31095b4357157e1676a9
tmarkec <tmarkec@gmail.com>
2023-05-14 15:21:46 +0000

    bug fix database setup, removed local version

--- home/views.py
+++ home/views.py
@@ -22,8 +22,4 @@ def privacy(request):
     return render(request, 'home/privacy.html')
 
 
-def handler404(request, exception):
-    """
-    This function shows a customized 404 error page
-     """
-    return render(request, 'errors/404.html', status=404)
+
